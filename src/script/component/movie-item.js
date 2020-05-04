b06a879940a39b2c7242aaf76982e045cdf16ddf
FirmanJabar <firmanabduljabar@gmail.com>
2020-05-04 22:17:07 +0800

    :fire: edit style and add responsive

--- src/script/component/movie-item.js
+++ src/script/component/movie-item.js
@@ -6,12 +6,11 @@ class MovieItem extends HTMLElement {
 
   render() {
     this.innerHTML = `
-      <div id="${this._movie.id}" class="card text-white bg-dark" type="button" data-toggle="modal" data-target="#exampleModalCenter">
-      <img class="card-img-top" src="${this._movie.poster_path !== null || undefined || "" ? "https://image.tmdb.org/t/p/w500"+this._movie.poster_path : "/src/img/null.png"}" alt="Poster Movie">
-          <div class="card-body">
-          <p class="card-title"><strong>${this._movie.original_title}</strong></p>
-          <p class="card-text">${this._movie.release_date ? this._movie.release_date : "Release Date - Not Found"}</p>
-          </div>
+      <div id="${this._movie.id}" class="card overflow-hidden rounded-4 text-center text-white bg-dark h-100" type="button" data-toggle="modal" data-target="#exampleModalCenter">
+      <img class="p-2 card-img rounded-4 card-img-top" src="${this._movie.poster_path !== null || undefined || "" ? "https://image.tmdb.org/t/p/w500"+this._movie.poster_path : "/src/img/null.png"}" alt="Poster Movie">
+        <div class="card-img-overlay d-flex flex-column justify-content-end">
+          <p class="grad1 card-title"><strong>${this._movie.original_title}</strong> (${this._movie.release_date ? this._movie.release_date.substring(0,4) : ""})</p>
+        </div>
       </div>
     `;
   }
